52fa76a38099e7712ae4ff1c2ecef0c959d509d1
Sara Zwairi <sara.zwairi@gmail.com>
2021-09-01 23:22:30 +0300

    add test

--- games/tests.py
+++ games/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.contrib.auth import get_user_model
 # Create your tests here.
+
+class game_crud_test(TestCase):
+    def setUp(self):
+        self.user=get_user_model().objects.create_user(
+            username='admin',
+            email='sara.zwairi@gmail.com',
+            password='sama'
+        )
+    def test_create_user(self):
+        self.assertEquals(self.user.email,'sara.zwairi@gmail.com')
+        self.assertEquals(self.user.username,'admin')
+
+    
